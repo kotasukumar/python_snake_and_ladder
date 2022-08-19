5405831c7d409f0b111636db6128c7c52c894259
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-08-19 10:51:27 +0530

    if negative start agin

--- snake_and_ladder.py
+++ snake_and_ladder.py
@@ -3,16 +3,19 @@ import random
 
 def play():
     player_position = 0
-    die = random.randint(1, 6)
-    play_option = random.randint(1, 3)
-    if play_option == 1:
-        pass
-    elif play_option == 2:
-        player_position -= die
-    elif play_option == 3:
-        player_position += die
+    while player_position <= 100:
+        die = random.randint(1, 6)
+        play_option = random.randint(1, 3)
+        if play_option == 1:
+            pass
+        elif play_option == 2:
+            player_position -= die
+        elif play_option == 3:
+            player_position += die
+        if play_option < 0:
+            player_position = 0
 
-    return play_option
+    return player_position
 
 
 if __name__ == "__main__":

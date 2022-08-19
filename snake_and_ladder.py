9748661581ef2c22a395f449ee2ce9a506838fd7
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-08-19 10:43:26 +0530

    option

--- snake_and_ladder.py
+++ snake_and_ladder.py
@@ -1,6 +1,20 @@
 import random
 
-print("Welcome to snake and ladder program")
-player = 0
-die = random.randint(1, 6)
-print(die)
+
+def play():
+    player_position = 0
+    die = random.randint(1, 6)
+    play_option = random.randint(1, 3)
+    if play_option == 1:
+        pass
+    elif play_option == 2:
+        player_position -= die
+    elif play_option == 3:
+        player_position += die
+
+    return play_option
+
+
+if __name__ == "__main__":
+    print("Welcome to snake and ladder program")
+    print(play())

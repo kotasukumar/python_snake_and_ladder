419ac085b6d3a95d7412a70e02ea646d8b15fe38
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-08-19 11:02:05 +0530

    need to stop at exact 100

--- snake_and_ladder.py
+++ snake_and_ladder.py
@@ -3,7 +3,7 @@ import random
 
 def play():
     player_position = 0
-    while player_position <= 100:
+    while player_position != 100:
         die = random.randint(1, 6)
         play_option = random.randint(1, 3)
         if play_option == 1:
@@ -12,8 +12,12 @@ def play():
             player_position -= die
         elif play_option == 3:
             player_position += die
-        if play_option < 0:
+        if player_position < 0:
             player_position = 0
+        elif player_position > 100:
+            player_position -= die
+        elif player_position == 100:
+            break
 
     return player_position
 
